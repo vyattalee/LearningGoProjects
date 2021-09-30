6ec403e0e0804fdf6167670a6ff5bdb5c8d40d97
jie li <jie.n.li@nokia-sbell.com>
2021-09-30 22:44:13 +0800

    update async_workflow.go it can build

--- closure/async_workflow.go
+++ closure/async_workflow.go
@@ -5,12 +5,12 @@ import "fmt"
 func main() {
 	a, b := 1, 2
 	// b := 2
-	go func() {
-		result := doWork1(a, b)
-		result = doWork2(result)
-		result = doWork3(result)
-		// Use the final result
-	}()
+	// go func() {
+	result := doWork1(a, b)
+	result = doWork2(result)
+	result = doWork3(result)
+	// Use the final result
+	// }()
 	fmt.Println("hi!")
 	fmt.Println("vim-go")
 }
@@ -19,12 +19,18 @@ func main() {
 //   // use the result here
 // })
 
-func doWork1() func(a int, b int) {
-	fmt.Println("doWork1: ", a, b)
+func doWork1(a int, b int) func() {
+	return func() {
+		fmt.Println("doWork1: ", a, b)
+	}
 }
-func doWork2() func(r func(int, int)) {
-	fmt.Println("doWork2: ", r)
+func doWork2(r func()) func() {
+	return func() {
+		fmt.Println("doWork2: ", r)
+	}
 }
-func doWork3() func(r func(int, int)) {
-	fmt.Println("doWork3: ", r)
+func doWork3(r func()) func() {
+	return func() {
+		fmt.Println("doWork3: ", r)
+	}
 }
