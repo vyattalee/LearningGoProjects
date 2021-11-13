5919f08f2832ef29e181110290758db72b2961b4
jienl <jie.n.li@nokia-sbell.com>
2021-11-13 14:57:11 +0800

    add Player & CangKuLan for Deck Game

--- src/DeckCard/Deck/Card.go
+++ src/DeckCard/Deck/Card.go
@@ -13,10 +13,33 @@ func NewCard(rank Rank, suit Suit) Card {
 		name:  SUITS[suit] + RANKS[rank]}
 }
 
+func (c *Card) SuitLevel() Suit {
+	return c._suit
+}
+
+func (c *Card) RankLevel() Rank {
+	return c._rank
+}
+
 func (c *Card) Suit() string {
 	return SUITS[c._suit]
 }
 
 func (c *Card) Rank() string {
 	return RANKS[c._rank]
+}
+
+// Info : Show human readable card info
+func (c Card) Info() (string, string) {
+	// translate card to human readable info
+
+	return c.Rank(), c.Suit()
+}
+
+// IsCardHigher : Compare 2 card, return true if card 1 number and symbol is higher
+func IsCardHigher(c1 Card, c2 Card) bool {
+	if c1.RankLevel() >= c2.RankLevel() && c1.SuitLevel() > c2.SuitLevel() {
+		return true
+	}
+	return false
 }
---
+++ src/DeckCard/Deck/Player.go
@@ -0,0 +1,83 @@
+//https://github.com/ibrdrahim/goRemi/blob/master/player.go
+//take goRemi as reference
+package Deck
+
+import (
+	"fmt"
+)
+
+// Player struct
+type Player struct {
+	Name     string
+	Score    float64
+	LastPlay Card
+	Hand     []Card
+	AI       bool // false player, true ai
+}
+
+// Less : sort player
+func (p Players) Less(i, j int) bool {
+	return p[i].Score < p[j].Score
+}
+
+// DrawCards : Draw card from deck
+func (p *Player) DrawCards(deck *Deck, numberOfDraw int) {
+	fmt.Printf("%s Draw %d card(s)\n", p.Name, numberOfDraw)
+	// draw card(s) from deck
+	draws := (*deck).DrawCards(numberOfDraw)
+	//sortCard(draws)
+	p.Hand = append(p.Hand, draws.Cards...)
+}
+
+// ThrowCards : Return card to deck
+func (p *Player) ThrowCards(cardIndex int, deck *Deck) {
+
+	if cardIndex < 0 || cardIndex > (len(p.Hand)-1) {
+		println("Invalid card index")
+		return
+	}
+
+	deck.AddCard(p.Hand[cardIndex])
+	p.LastPlay = p.Hand[cardIndex]
+	p.Hand = append(p.Hand[:cardIndex], p.Hand[cardIndex+1:]...)
+
+	var cName, cLevel = p.LastPlay.Info()
+	fmt.Printf("\n\n%s Play %s %s", p.Name, cLevel, cName)
+}
+
+// ShowHand : show human readable info of cards in player hand
+func (p Player) ShowHand() {
+	for i := 0; i < len(p.Hand); i++ {
+
+		var cName, cLevel = p.Hand[i].Info()
+		fmt.Printf("Card Index %d \n", i+1)
+		fmt.Print("┌───┐\n")
+		fmt.Printf("|%v %v|\n", cLevel, cName)
+		fmt.Print("└───┘\n")
+
+		if i < len(p.Hand)-1 {
+			//fmt.Printf(",")
+		}
+
+	}
+}
+
+// Players : collection of player
+type Players []Player
+
+// Register : Register Player
+func Register(pl []string, numberOfAI int) Players {
+
+	var numberOfPlayers = len(pl) + numberOfAI
+	players := make(Players, numberOfPlayers)
+
+	for index := 0; index < len(pl)+numberOfAI; index++ {
+		if index <= len(pl)-1 {
+			players[index] = Player{Name: pl[index], Score: 0, AI: false}
+		} else {
+			players[index] = Player{Name: fmt.Sprintf("Computer %d", index), Score: 0, AI: true}
+		}
+	}
+
+	return players
+}
