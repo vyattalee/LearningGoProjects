0220424cdc71311852b21370a6243313abb87635
jie li <jie.n.li@nokia-sbell.com>
2021-11-29 18:06:06 +0800

    add the LDFlags example into project

---
+++ src/HowToCodeinGo/LDFlagsExample/main.go
@@ -0,0 +1,18 @@
+package main
+
+//go build -v -ldflags="-X 'main.Version=v1.0.0' -X
+//'build/build.User=$(id -u -n)' -X
+//'build/build.Time=$(date)'"
+
+import (
+	"build"
+	"fmt"
+)
+
+var Version = "development"
+
+func main() {
+	fmt.Println("Version:\t", Version)
+	fmt.Println("build.Time:\t", build.Time)
+	fmt.Println("build.User:\t", build.User)
+}
