a5ed63f19a80005ca2c94eb5c1e9274df95eb72f
jie li <jie.n.li@nokia-sbell.com>
2022-01-07 17:49:33 +0800

    add streaming GRPC in projects, but not completely

--- src/ResourceMonitor/service/processors_server.go
+++ src/ResourceMonitor/service/processors_server.go
@@ -21,12 +21,23 @@ func NewProcessorsServer() *ProcessorsServer {
 	return &ProcessorsServer{}
 }
 
+func GetCpuInfo() ([]cpu.InfoStat, error) {
+	cpuInfos, err := cpu.Info()
+	if err != nil {
+		fmt.Printf("get cpu info failed, err:%v", err)
+		return nil, err
+	}
+
+	return cpuInfos, nil
+
+}
+
 func GetCpuPercent() float64 {
 	percent, _ := cpu.Percent(time.Second, false)
 	return percent[0]
 }
 
-// CreateLaptop is a unary RPC to create a new laptop
+// GetProcessorsInfo is a unary RPC to get processors info
 func (server *ProcessorsServer) GetProcessorsInfo(
 	ctx context.Context,
 	req *pb.GetProcessorsRequest,
@@ -34,19 +45,47 @@ func (server *ProcessorsServer) GetProcessorsInfo(
 	request := req.String()
 	log.Printf("service get local processors info %s", request)
 
-	info, _ := cpu.Info() //总体信息
-	fmt.Println(info)
+	cpuInfos, _ := GetCpuInfo() //总体信息
 	//output：	[{"cpu":0,cores":4,"modelName":"Intel(R) Core(TM) i5-2520M CPU @ 2.50GHz","mhz":2501,。。。]
 
-	ci := &pb.CPU{
-		Name:          info[0].ModelName,
-		Brand:         info[0].VendorID,
-		NumberCores:   uint32(info[0].Cores),
-		NumberThreads: uint32(info[0].Cores),
-		MaxGhz:        info[0].Mhz,
-		MinGhz:        info[0].Mhz,
+	if cpuInfos == nil {
+		return nil, fmt.Errorf("no processors info")
+	}
+
+	var ci []*pb.CPU
+	for i, c := range cpuInfos {
+		if i < len(cpuInfos) {
+
+			ci[i] = &pb.CPU{
+				CpuId:       c.CPU,
+				VendorId:    c.VendorID,
+				ModelName:   c.ModelName,
+				Mhz:         c.Mhz,
+				CacheSize:   c.CacheSize,
+				Flags:       c.Flags,
+				UsedPercent: GetCpuPercent(),
+			}
+		}
 	}
 
 	res := &pb.GetProcessorsResponse{Cpu: ci}
+	return res, nil
+
+}
+
+type ProcessorsServiceSubscribeProcesssorsInfoServer struct {
+	pb.ProcessorsService_SubscribeProcesssorsInfoServer
+}
+
+//SubscribeProcessorsInfo is stream RPC to get processors info
+func (server *ProcessorsServiceSubscribeProcesssorsInfoServer) SubscribeProcesssorsInfo(
+	ctx context.Context,
+	req *pb.GetProcessorsRequest,
+) (*pb.GetProcessorsResponse, error) {
+
+	request := req.String()
+	log.Printf("service subscribe local processors info %s", request)
+
+	res := &pb.GetProcessorsResponse{}
 	return res, nil
 }
