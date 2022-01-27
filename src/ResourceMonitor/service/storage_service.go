cc40735e0871f8c3e6b1b2bce6a62443053ca9d2
vyattalee <jie.n.li@nokia-sbell.com>
2022-01-27 22:16:37 +0800

    update storage service in project. it does NOT finish!

--- src/ResourceMonitor/service/storage_service.go
+++ src/ResourceMonitor/service/storage_service.go
@@ -15,7 +15,7 @@ func getStorageInfo() (*pb.Storage, error) {
 	ioCountinfo, err3 := disk.IOCounters() //所有硬盘的io信息
 	fmt.Println(ioCountinfo)
 
-	return &pb.Storage{ /*Partition: partitionsInfo, Usage:usageInfo, IoCount:ioCountinfo*/ }, fmt.Errorf("%s %s %s", err1, err2, err3)
+	return &pb.Storage{ /*Partition: partitionsInfo, Usage:usageInfo, IoCount:ioCountinfo*/ }, fmt.Errorf("%v %v %v", err1, err2, err3)
 	//[{"device":"C:","mountpoint":"C:","fstype":"NTFS","opts":"rw.compress"} {"device":"D:","mountpoint":"D:","fstype":"NTFS","opts":"rw.compress"} {"device":"E:","mountpoint":"E:","fstype":"NTFS","opts":"rw.compress"} ]
 	//{"path":"E:","fstype":"","total":107380965376,"free":46790828032,"used":60590137344,"usedPercent":56.425398236866755,"inodesTotal":0,"inodesUsed":0,"inodesFree":0,"inodesUsedPercent":0}
 	//map[C::{"readCount":0,"mergedReadCount":0,"writeCount":0,"mergedWriteCount":0,"readBytes":0,"writeBytes":4096,"readTime":0,"writeTime":0,"iopsInProgress":0,"ioTime":0,"weightedIO":0,"name":"C:","serialNumber":"","label":""} 。。。]
